d2c5dcbc3b1d680baae1c238db7c3129fee50f4b
Domodiak <kkv84271@gmail.com>
2023-06-01 16:55:38 +0300

    Set up sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
@@ -6,6 +6,7 @@ function App() {
       <h1>You should be able to properly communicate <span className='accent'>here</span> soon.</h1>
       <button className='btn-primary'>Button</button>
       <button className='btn-secondary'>Button</button>
+      <div className={styles['h6']}>Hi</div>
     </div>
   );
 }
